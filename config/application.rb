8f45100aaf2c2284ba8984f935c7b92c9f5d185c
Nguyen <renkyrou1708@gmail.com>
2023-02-16 03:19:46 -0800

    Add Users Schema

--- config/application.rb
+++ config/application.rb
@@ -7,6 +7,7 @@ Bundler.require(*Rails.groups)
 module BaseApi
   class Application < Rails::Application
     config.load_defaults 7.0
+    config.time_zone = "Hanoi"
     config.api_only = true
   end
 end
